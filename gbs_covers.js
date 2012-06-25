849f259fefa8a29c0cd6ee9e223a463d54f8c321
Academic Technology and Information Systems <olis@sysadm.suny.edu>
2012-06-25 11:52:17 -0300

    Update master

--- gbs_covers.js
+++ gbs_covers.js
@@ -1,11 +1,55 @@
-var MAXBUFFER=10;function gbook_coverart(data){for(i in data){var book=data[i];if(book.thumbnail_url!=undefined){updateCover(book.bib_key.replace(':','_'),book);}}}
-function gbookScript(bibkeys,callback){var script=document.createElement("script");src="http://books.google.com/books?jscmd=viewapi&bibkeys="+escape(bibkeys.join(','))+"&callback="+callback;script.src=src;document.body.appendChild(script);}
-function updateCover(bibkey,book){gbook_div=document.getElementById(bibkey);if(gbook_div.className=='gbookthumbnail'&&book.thumbnail_url){gbook_div.innerHTML='<img src="'+book.thumbnail_url+'" border="0" alt="" />';}else if(gbook_div.className=='gbookpreview'&&book.preview_url){link_text='More Info';if(book.preview=='partial'){link_text='Preview';}
-if(book.preview=='full'){link_text='Full Text';}
-gbook_div.innerHTML='<a href="'+book.preview_url+'" class="thumbnail" target="_blank">'
-+'<img src="'+book.thumbnail_url+'" border="0" alt="" />'
-+'<span style="padding-top: 3px; display: block;">'+link_text+'</span></a>';}else if(gbook_div.className=='gbookfull'){}}
-function parseBibkey(bibkey){if(!bibkey)return'';return bibkey.replace(/[^\dXx]/g,'');}
-window.onload=function(){allDivs=document.getElementsByTagName('div');bibkeys=[];for(i in allDivs){var div=allDivs[i];if(div.className=='gbookthumbnail'||div.className=='gbookpreview'||div.className=='gbsthumbnail'){var bibkey=parseBibkey(div.getAttribute('bibkey'));if(bibkey!=''){bibkeys.push(bibkey);div.setAttribute('id',bibkey.replace(':','_'));div.innerHTML='<img src="http://www.sunyconnect.suny.edu/gbs_cover_art/cover.png" height="80" border="0" alt="" />';}}
-if(bibkeys.length>=MAXBUFFER){gbookScript(bibkeys,'gbook_coverart');bibkeys=[];}}
-if(bibkeys.length>0){gbookScript(bibkeys,'gbook_coverart');}}
+(function() {
+  var CoverArt, CoverImageSearch;
+  var __slice = Array.prototype.slice;
+  CoverImageSearch = (function() {
+    var BASE_URL, DEFAULT_ALLOWED_NAMES, MAX_BUFFER_SIZE;
+    MAX_BUFFER_SIZE = 10;
+    BASE_URL = 'http://coverartlocator.herokuapp.com/search';
+    DEFAULT_ALLOWED_NAMES = ['gbookthumbnail', 'gbookpreview', 'gbsthumbnail'];
+    function CoverImageSearch() {
+      this.bibkeys = [];
+      this.allowed_names = DEFAULT_ALLOWED_NAMES;
+    }
+    CoverImageSearch.prototype.addBibkey = function(div) {
+      var bibkey;
+      if (this.isAllowedName(div.className)) {
+        if (bibkey = this.parseBibkey(div.getAttribute('bibkey'))) {
+            if(bibkey.search(":") > 0){
+               bibkey = bibkey.toLowerCase();  
+            }
+            else{
+               bibkey = "isbn:" +  bibkey
+            }
+          this.bibkeys.push(bibkey);
+          this.insertPlaceholder(div, bibkey);
+          if (this.bibkeys.length >= MAX_BUFFER_SIZE) {
+            return this.submit();
+          }
+        }
+      }
+    };
+    CoverImageSearch.prototype.submit = function() {
+      if (this.bibkeys.length > 0) {
+        this.insertScriptTag(document.body);
+      }
+      return this.bibkeys = [];
+    };
+    CoverImageSearch.prototype.insertScriptTag = function(ele) {
+      var script;
+      script = document.createElement('script');
+      script.src = "" + BASE_URL + "?q=" + (escape(this.bibkeys.join(','))) + "&callback=gbook_coverart";
+      return ele.appendChild(script);
+    };
+    CoverImageSearch.prototype.insertPlaceholder = function(ele, key) {
+      var img;
+      ele.id = key.replace(':', '_');
+      img = document.createElement('img');
+      img.src = 'http://www.sunyconnect.suny.edu/gbs_cover_art/cover.png';
+      img.height = '80';
+      img.border = '0';
+      img.alt = '';
+      return ele.appendChild(img);
+    };
+    CoverImageSearch.prototype.parseBibkey = function(id) {
+      if (!id) {
+        return '';
